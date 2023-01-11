00f3d7d14e30af041709c97eedfff84150806469
Evgeny Loy <evgenyloy@gmail.com>
2023-01-11 23:03:59 +0300

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,65 @@
+import { Component } from 'react'
+import './App.css'
+import Display from '../display/Display'
+import Form from '../form/Form'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      error: '',
+      isLoading: true,
+      currency: {},
+      usd: '',
+      eur: '',
+      gbp: '',
+    }
+  }
+
+  componentDidMount() {
+    this.getState()
+  }
+
+  getState = async () => {
+    try {
+      const API_URL = 'https://www.cbr-xml-daily.ru/daily_json.js'
+      const res = await fetch(API_URL)
+      const data = await res.json()
+
+      this.setState({ currency: data })
+      this.setState({ usd: data.Valute.USD.Value.toFixed(2) })
+      this.setState({ eur: data.Valute.EUR.Value.toFixed(2) })
+      this.setState({ gbp: data.Valute.GBP.Value.toFixed(2) })
+    } catch (error) {
+      this.setState({ error: error.message })
+    }
+    this.setState({ isLoading: false })
+  }
+
+  render() {
+    if (this.state.error) {
+      return <h1>Error: {this.state.error}</h1>
+    }
+
+    return (
+      <div className="converter">
+        <div className="converter__title">Currency Converter</div>
+        {this.state.isLoading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <Display
+            usd={this.state.usd}
+            eur={this.state.eur}
+            gbp={this.state.gbp}
+            currency={this.state.currency}
+          />
+        )}
+
+        <Form usd={this.state.usd} eur={this.state.eur} gbp={this.state.gbp} />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/form/Form.js
@@ -0,0 +1,73 @@
+import { Component } from 'react'
+
+class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      input1: '',
+      input2: '',
+      valueSelected: 'usd',
+    }
+  }
+
+  onValueChange = (e) => {
+    this.setState({ input1: e.target.value })
+  }
+
+  onSelect = (e) => {
+    this.setState({ valueSelected: e.target.value })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevState.input1 !== this.state.input1 ||
+      prevState.valueSelected !== this.state.valueSelected
+    ) {
+      let a = (
+        Number(this.state.input1) / this.props[this.state.valueSelected]
+      ).toFixed(2)
+      this.setState({ input2: a === 'NaN' ? 0 : a })
+    }
+  }
+
+  render() {
+    return (
+      <form action="#" className="converter__form-inner">
+        <div className="converter__form">
+          <select className="converter__form-select">
+            <option value="RUB">RUB</option>
+          </select>
+          <input
+            className="converter__form-input"
+            id="input1"
+            type="number"
+            placeholder="0"
+            value={this.state.input1}
+            onChange={this.onValueChange}
+          />
+        </div>
+        <div className="converter__form">
+          <select
+            className="converter__form-select"
+            id="select"
+            onChange={this.onSelect}
+            value={this.valueSelected}
+          >
+            <option value="usd">USD</option>
+            <option value="eur">EUR</option>
+            <option value="gbp">GPB</option>
+          </select>
+          <input
+            className="converter__form-input"
+            id="input2"
+            type="text"
+            readOnly
+            value={this.state.input2}
+          />
+        </div>
+      </form>
+    )
+  }
+}
+
+export default Form
